14edb7c62ff3a5c869c5b01a9619228af3fbdd04
LAKSHMIKANTH VALLAMPATI <kanth.vallampati@gmail.com>
2014-01-31 12:08:19 +0530

    Update update.php

--- php_mysql_cruds/update.php
+++ php_mysql_cruds/update.php
@@ -1,6 +1,6 @@
 <?php
 
-	include('class/mysql_crud.php');
+	include('includes/actions.php');
 	global $sadb;
 	
 	$sadb->update('test',array('name'=>"Name 4",'email'=>"name4@email.com"),'id="1" AND name="Name 1"'); // Table name, column names and values, WHERE conditions
